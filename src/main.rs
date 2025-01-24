f41137eab09a1a6cc0bf8dd90f850fb569fa6582
Robin TONCOURT <robin.toncourt@gmail.com>
2025-01-24 18:18:53 +0100

    Removing main.rs code.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,3 @@
 #![allow(dead_code)]
-mod strings;
-mod utils;
-use strings::correcteur::create_trie_from_word_list;
 
-fn main() {
-    let words = vec!["as", "port", "pore", "pre", "pres", "pret"];
-
-    let trie = create_trie_from_word_list(&words);
-    println!("{trie:#?}");
-}
+fn main() {}
