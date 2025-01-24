391cfab8c64525481ee3655d61fcf2e49b0ccd78
Robin TONCOURT <robin.toncourt@gmail.com>
2025-01-25 00:52:44 +0100

    Knuth Morris and Pratt pattern research implementation.

--- src/strings/knuth_morris_pratt.rs
+++ src/strings/knuth_morris_pratt.rs
@@ -1,12 +1,72 @@
 // Complexité de O(m + n), somme des longeurs du mot et du pattern.
 fn knuth_morris_pratt(word: &str, pattern: &str) -> Option<usize> {
-    todo!()
+    if pattern.is_empty() {
+        return Some(0);
+    }
+
+    let word_letters: Vec<char> = word.chars().collect();
+    let pattern_letters: Vec<char> = pattern.chars().collect();
+
+    let mut r: Vec<isize> = vec![0; pattern.len()];
+    r[0] = -1;
+    let mut j: isize = -1;
+
+    for i in 1..pattern.len() {
+        while j >= 0 && pattern_letters[i - 1] != pattern_letters[j as usize] {
+            j = r[j as usize];
+        }
+        j += 1;
+        r[i] = j;
+    }
+    j = 0;
+    for (i, c) in word_letters.iter().enumerate() {
+        while j >= 0 && *c != pattern_letters[j as usize] {
+            j = r[j as usize];
+        }
+        j += 1;
+        if j == pattern.len() as isize {
+            return Some(i + 1 - pattern.len())
+        }
+    }
+
+    None
 }
 
-fn knuth_morris_pratt_personnal(word: &str, pattern: &str) -> Option<usize> {
-    todo!()
+fn one_loop(word: &str, pattern: &str) -> Option<usize> {
+    if word.len() < pattern.len() {
+        return None;
+    } else if pattern.is_empty() {
+        return Some(0);
+    }
+
+    let word_letters: Vec<char> = word.chars().collect();
+    let pattern_letters: Vec<char> = pattern.chars().collect();
+
+    let mut result: usize = 0;
+    let mut pattern_i: usize = 0;
+    let mut first_match: bool = true;
+    for (i, c) in word_letters.iter().enumerate() {
+        if *c == pattern_letters[pattern_i] {
+            if first_match {
+                result = i;
+                first_match = false;
+            }
+
+            pattern_i += 1;
+            if pattern_i == pattern_letters.len() {
+                return Some(result);
+            }
+        } else {
+            result = i;
+            pattern_i = 0;
+            first_match = true;
+        }
+    }
+
+    None
 }
 
+#[allow(clippy::manual_find)]
 fn naive(word: &str, pattern: &str) -> Option<usize> {
     if word.len() < pattern.len() {
         return None;
@@ -33,12 +93,15 @@ mod knuth_morris_pratt_test {
     use super::*;
 
     fn compare_tester(function: &dyn Fn(&str, &str) -> Option<usize>) {
+        assert_eq!(Some(6), function("lalopalalali", "lala"));
         assert_eq!(None, function("bon", "bonjour"));
         assert_eq!(Some(0), function("bonjour", "bon"));
         assert_eq!(Some(3), function("bonjour", "jour"));
         assert_eq!(None, function("bonjour", "bonsoir"));
         assert_eq!(None, function("", "bonjour"));
         assert_eq!(Some(0), function("bonjour", ""));
+        assert_eq!(Some(0), function("", ""));
+        assert_eq!(Some(2), function("lalalali", "lalali"));
     }
 
     #[test]
@@ -47,8 +110,8 @@ mod knuth_morris_pratt_test {
     }
 
     #[test]
-    fn test_knuth_morris_pratt_personnal() {
-        compare_tester(&knuth_morris_pratt_personnal);
+    fn test_one_loop() {
+        compare_tester(&one_loop);
     }
 
     #[test]
